fe8f1a5f22cfa80019ce56e91486520f2a3a2e35
shahnawajsafin-cmyk <shahanawajsafin@gmail.com>
2025-11-02 23:30:53 +0600

    Create EmployeeDetails.java

---
+++ EmployeeDetails.java
@@ -0,0 +1,17 @@
+package com.mycompany.employeedetails;
+
+public class EmployeeDetails {
+
+    public static void main(String[] args) {
+        
+        String name="Habibur Rahman";
+        int id=1143;
+        double salary=2000;
+        double annualsalary=salary*12;
+        
+        System.out.println("name= "+name);
+        System.out.println("id= "+id);
+        System.out.println("salary= "+salary);
+        System.out.println("annualsalary= "+annualsalary);
+    }
+}
